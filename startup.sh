35c4e84285cdf65972926eddaf6596a0b27b58f0
whjin <wuhuajin09@163.com>
2026-05-16 22:00:02 +0800

    action: 新增一键提交、一键开启服务脚本

---
+++ startup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+PROJECT_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+read -p "请输入要执行的文件名：" filename
+
+start "" "git-bash.exe" --cd="$PROJECT_ROOT/server" -c "node $filename;exec bash"
+
+pnpm dev
