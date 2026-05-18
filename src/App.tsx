444250b950c7d6a984a2157ec684e0af3001edf3
whjin <wuhuajin09@163.com>
2026-05-18 17:21:18 +0800

    提交更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import './styles/index.scss';
 import logo from './assets/images/react.svg';
@@ -7,24 +8,44 @@ import robotsdata from './mock/robots.json';
 import robotStyles from './components/Robot/Robot.module.scss';
 import ShoppingCart from './components/Robot/ShoppingCart';
 
-function App() {
-  return (
-    <>
-      <AIApp />
-      <div className={robotStyles.robotContainer}>
-        <header>
-          <img src={logo} className={robotStyles.logo} alt='logo' />
-          <h1 className={robotStyles.title}>可跨网络通信，支持本机与远程进程交互，后端服务器与前端服务端交互</h1>
-        </header>
-        <ShoppingCart />
-        <ul className={robotStyles.list}>
-          {robotsdata.map(r => (
-            <Robot id={r.id} name={r.name} email={r.email} key={r.id} />
-          ))}
-        </ul>
-      </div>
-    </>
-  );
+interface Props {}
+
+interface State {
+  robotGallary: any[];
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      robotGallary: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => this.setState({ robotGallary: data }));
+  }
+  render() {
+    return (
+      <>
+        <AIApp />
+        <div className={robotStyles.robotContainer}>
+          <header>
+            <img src={logo} className={robotStyles.logo} alt="logo" />
+            <h1 className={robotStyles.title}>可跨网络通信，支持本机与远程进程交互，后端服务器与前端服务端交互</h1>
+          </header>
+          <ShoppingCart />
+          <ul className={robotStyles.list}>
+            {this.state.robotGallary.map((r) => (
+              <Robot id={r.id} name={r.name} email={r.email} key={r.id} />
+            ))}
+          </ul>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Robot/ShoppingCart.tsx
+++ src/components/Robot/ShoppingCart.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './ShoppingCard.module.scss';
+import { FaShoppingCart } from 'react-icons/fa';
 
 interface Props {}
 
@@ -15,11 +16,22 @@ class ShoppingCart extends React.Component<Props, State> {
     };
   }
 
+  toggleCart = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if ((e.target as HTMLElement).nodeName === 'SPAN') {
+      this.setState({
+        isOpen: !this.state.isOpen,
+      });
+    }
+  };
+
   render() {
     return (
       <div className={styles.shoppingCart}>
-        <button>购物车 2 （件）</button>
-        <div>
+        <button onClick={this.toggleCart}>
+          <FaShoppingCart />
+          <span>购物车 2 （件）</span>
+        </button>
+        <div style={{ display: this.state.isOpen ? 'block' : 'none' }}>
           <ul>
             <li>robot 1</li>
             <li>robot 2</li>
